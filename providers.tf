6131944b0b7e2dfd935466acee17a3c64da105c2
alina <alinatuzenko@gmail.com>
2024-09-23 19:01:29 -0500

    add providers.tf

---
+++ providers.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+    region = "us-east-1"
+    #profile = "default"     #instead of profile you can add access_key and secret_key. to find them you need to run cat ~/.aws/credentials
+}
